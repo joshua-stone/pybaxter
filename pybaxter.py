b3967e0bcbee0a263b44217f39c7e2f630bf0aab
Joshua Stone <joshua.gage.stone@gmail.com>
2017-06-02 09:19:11 -0400

    Restructure way joint information is stored in a more useful form

--- pybaxter.py
+++ pybaxter.py
@@ -5,26 +5,6 @@ from rospy import init_node
 from sys import exit
 
 class Baxter(object):
-    _arm_joints = {
-        'left': [
-            'left_s0',
-            'left_s1',
-            'left_e0',
-            'left_e1',
-            'left_w0',
-            'left_w1',
-            'left_w2'
-        ],
-        'right': [
-            'right_s0',
-            'right_s1',
-            'right_e0',
-            'right_e1',
-            'right_w0',
-            'right_w1',
-            'right_w2'
-        ]
-    }
     def __init__(self, name='Baxter_Node'):
 	self._name = name
 	init_node(self._name)
@@ -33,13 +13,21 @@ class Baxter(object):
 	    'left': Limb('left'),
             'right': Limb('right')
         }
-		 
-    def set_angles_to_zero(self, side):
-        angles = {joint: 0.0 for joint in self._arm_joints[side]}
+
+    def reset_limb(self, side):
+        angles = {joint: 0.0 for joint in self._limbs[side].joint_angles()}
 	self._limbs[side].move_to_joint_positions(angles)
+
+    def joints(self):
+        joints = {
+            'left': self._limbs['left'].joint_angles(),
+            'right': self._limbs['right'].joint_angles()
+        }
+        return joints
 
 if __name__ == '__main__':
     node = Baxter()
-    node.set_angles_to_zero('right')
-
+    node.reset_limb('left')
+    node.reset_limb('right')
+    print(node.joints())
     exit()
