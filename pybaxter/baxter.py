61aed65943c90f99ec509a7a62867b9d82598ddc
Joshua Stone <joshua.gage.stone@gmail.com>
2017-06-03 01:25:59 -0400

    Fix mixing of spaces and tabs

--- pybaxter/baxter.py
+++ pybaxter/baxter.py
@@ -13,17 +13,17 @@ LEFT, RIGHT = 'left', 'right'
 
 class Baxter(object):
     def __init__(self, name='Baxter_Node'):
-	self._name = name
-	init_node(self._name)
-	RobotEnable().enable()
-	self._limbs = {
-	    LEFT: Limb(LEFT),
+        self._name = name
+        init_node(self._name)
+        RobotEnable().enable()
+        self._limbs = {
+            LEFT: Limb(LEFT),
             RIGHT: Limb(RIGHT)
         }
 
     def reset_limb(self, side):
         angles = {joint: 0.0 for joint in self._limbs[side].joint_angles()}
-	self._limbs[side].move_to_joint_positions(angles)
+        self._limbs[side].move_to_joint_positions(angles)
 
     def joints(self):
         joints = {limb: joint.joint_angles() for limb, joint in self._limbs.iteritems()}
