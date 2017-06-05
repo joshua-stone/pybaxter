a54af2248b4de1a7eb30d3d870c20ca1df41dccc
Joshua Stone <joshua.gage.stone@gmail.com>
2017-06-04 23:20:40 -0400

    Expose information about Baxter's state and joints through properties

--- pybaxter/baxter.py
+++ pybaxter/baxter.py
@@ -15,7 +15,9 @@ class Baxter(object):
     def __init__(self, name='Baxter_Node'):
         self._name = name
         init_node(self._name)
-        RobotEnable().enable()
+ 
+        self._baxter_state = RobotEnable()
+
         self._limbs = {
             LEFT: Limb(LEFT),
             RIGHT: Limb(RIGHT)
@@ -29,3 +31,48 @@ class Baxter(object):
         joints = {limb: joint.joint_angles() for limb, joint in self._limbs.iteritems()}
         return joints
 
+    @property
+    def enabled(self):
+        return self._baxter_state.state().enabled
+    @property
+    def left_s0(self):
+        return self._limbs[LEFT].joint_angles()['left_s0']
+    @property
+    def left_s1(self):
+        return self._limbs[LEFT].joint_angles()['left_s1']
+    @property
+    def left_e0(self):
+        return self._limbs[LEFT].joint_angles()['left_e0']
+    @property
+    def left_e1(self):
+        return self._limbs[LEFT].joint_angles()['left_e1']
+    @property
+    def left_w0(self):
+        return self._limbs[LEFT].joint_angles()['left_w0']
+    @property
+    def left_w1(self):
+        return self._limbs[LEFT].joint_angles()['left_w1']
+    @property
+    def left_w2(self):
+        return self._limbs[LEFT].joint_angles()['left_w2']
+    @property
+    def right_s0(self):
+        return self._limbs[RIGHT].joint_angles()['right_s0']
+    @property
+    def right_s1(self):
+        return self._limbs[RIGHT].joint_angles()['right_s1']
+    @property
+    def right_e0(self):
+        return self._limbs[RIGHT].joint_angles()['right_e0']
+    @property
+    def right_e1(self):
+        return self._limbs[RIGHT].joint_angles()['right_e1']
+    @property
+    def right_w0(self):
+        return self._limbs[RIGHT].joint_angles()['right_w0']
+    @property
+    def right_w1(self):
+        return self._limbs[RIGHT].joint_angles()['right_w0']
+    @property
+    def right_w2(self):
+        return self._limbs[RIGHT].joint_angles()['right_w0']
