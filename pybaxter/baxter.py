c3e85ea1e055e823b399c1d25546cbb02f15c967
Joshua Stone <joshua.gage.stone@gmail.com>
2017-06-05 01:11:56 -0400

    Using properties extensively makes Baxter API easier to work with

--- pybaxter/baxter.py
+++ pybaxter/baxter.py
@@ -18,9 +18,12 @@ class Baxter(object):
  
         self._baxter_state = RobotEnable()
 
+        self._left = Limb(LEFT)
+        self._right = Limb(RIGHT)
+
         self._limbs = {
-            LEFT: Limb(LEFT),
-            RIGHT: Limb(RIGHT)
+            LEFT: self._left,
+            RIGHT: self._right
         }
 
     def reset_limb(self, side):
@@ -41,45 +44,185 @@ class Baxter(object):
     @property
     def enabled(self):
         return self._baxter_state.state().enabled
+
     @property
     def left_s0(self):
-        return self._limbs[LEFT].joint_angles()['left_s0']
+        return self._left.joint_angles()['left_s0']
+
+    @left_s0.setter
+    def left_s0(self, angle):
+        joints = self._left.joint_angles()
+        joints['left_s0'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def left_s1(self):
-        return self._limbs[LEFT].joint_angles()['left_s1']
+        return self._left.joint_angles()['left_s1']
+
+    @left_s1.setter
+    def left_s1(self, angle):
+        joints = self._left.joint_angles()
+        joints['left_s1'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def left_e0(self):
-        return self._limbs[LEFT].joint_angles()['left_e0']
+        return self._left.joint_angles()['left_e0']
+
+    @left_e0.setter
+    def left_e0(self, angle):
+        joints = self._left.joint_angles()
+        joints['left_e0'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def left_e1(self):
-        return self._limbs[LEFT].joint_angles()['left_e1']
+        return self._left.joint_angles()['left_e1']
+
+    @left_e1.setter
+    def left_e1(self, angle):
+        joints = self._left.joint_angles()
+        joints['left_e1'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def left_w0(self):
-        return self._limbs[LEFT].joint_angles()['left_w0']
+        return self._left.joint_angles()['left_w0']
+
+    @left_w0.setter
+    def left_w0(self, angle):
+        joints = self._left.joint_angles()
+        joints['left_w0'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def left_w1(self):
-        return self._limbs[LEFT].joint_angles()['left_w1']
+        return self._left.joint_angles()['left_w1']
+
+    @left_w1.setter
+    def left_w1(self, angle):
+        joints = self._left.joint_angles()
+        joints['left_w1'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def left_w2(self):
-        return self._limbs[LEFT].joint_angles()['left_w2']
+        return self._left.joint_angles()['left_w2']
+
+    @left_w2.setter
+    def left_w2(self, angle):
+        joints = self._left.joint_angles()
+        joints['left_w2'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def right_s0(self):
-        return self._limbs[RIGHT].joint_angles()['right_s0']
+        return self._right.joint_angles()['right_s0']
+
+    @right_s0.setter
+    def right_s0(self, angle):
+        joints = self._right.joint_angles()
+        joints['right_s0'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def right_s1(self):
-        return self._limbs[RIGHT].joint_angles()['right_s1']
+        return self._right.joint_angles()['right_s1']
+
+    @right_s1.setter
+    def right_s1(self, angle):
+        joints = self._right.joint_angles()
+        joints['right_s1'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def right_e0(self):
-        return self._limbs[RIGHT].joint_angles()['right_e0']
+        return self._right.joint_angles()['right_e0']
+
+    @right_e0.setter
+    def right_e0(self, angle):
+        joints = self._right.joint_angles()
+        joints['right_e0'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def right_e1(self):
-        return self._limbs[RIGHT].joint_angles()['right_e1']
+        return self._right.joint_angles()['right_e1']
+
+    @right_e1.setter
+    def right_e1(self, angle):
+        joints = self._right.joint_angles()
+        joints['right_e1'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def right_w0(self):
-        return self._limbs[RIGHT].joint_angles()['right_w0']
+        return self._right.joint_angles()['right_w0']
+
+    @right_w0.setter
+    def right_w0(self, angle):
+        joints = self._right.joint_angles()
+        joints['right_w0'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def right_w1(self):
-        return self._limbs[RIGHT].joint_angles()['right_w1']
+        return self._right.joint_angles()['right_w1']
+
+    @right_w1.setter
+    def right_w1(self, angle):
+        joints = self._right.joint_angles()
+        joints['right_w1'] = angle
+
+        self.enable_check()
+
+        self._left.move_to_joint_positions(joints)
+
     @property
     def right_w2(self):
-        return self._limbs[RIGHT].joint_angles()['right_w2']
+        return self._right.joint_angles()['right_w2']    
+
+    @right_w2.setter
+    def right_w2(self, angle):
+        joints = self._right.joint_angles()
+        joints['right_w2'] = angle
+
+        self.enable_check()
+        self._left.move_to_joint_positions(joints)
+
